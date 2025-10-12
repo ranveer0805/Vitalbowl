e262238369baa07add89393a6feee28683a22aaa
Tanya <12hellotanya@gmail.com>
2025-10-12 21:25:34 +0530

    Update frontend BASE_URL to match the correct backend URL

--- Frontend/src/utils/url.js
+++ Frontend/src/utils/url.js
@@ -1,4 +1,4 @@
-// // export const BASE_URL = "https://moneytrail-expense-tracker-app.onrender.com/api/v1";
+// export const BASE_URL = "https://moneytrail-expense-tracker-app.onrender.com/api/v1";
 // export const BASE_URL = "http://localhost:8000/api/v1";
 
-export const BASE_URL = "https://vitalbowl-backend.onrender.com/api";
+export const BASE_URL = "https://vitalbowl.onrender.com/api";
